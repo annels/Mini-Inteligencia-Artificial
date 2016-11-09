9347939eb734e616d638b2f1289c65bc793e69ff
annels <annelinterfase@gmail.com>
2016-11-08 18:41:06 -0600

    agregando-complementos

---
+++ js/enviar.js
@@ -0,0 +1,61 @@
+
+
+
+$('#enviar').click(function() {
+
+	var persona = $('#opciones');
+
+	
+    if ( opciones.value == "hola") {
+           alert ("¿Qué tranza?");}
+		   
+		   
+	if ( opciones.value == "adios") {
+		   alert ("¡Pues bay!") ; }
+		   
+		   
+	if ( opciones.value == "que pedo") {
+		   alert ("Uy!") ; 	}
+		   
+	if ( opciones.value == "te amo") {
+		  alert ("Uououo ¡tranquilo viejo!") ; }    
+		   
+		    	   
+
+});
+
+
+function regresar(){
+    window.location.reload();
+} 
+
+
+
+
+
+
+function runScript(e) {
+    if (e.keyCode == 13) {
+
+	var persona = $('#opciones');
+
+	
+    if ( opciones.value == "hola") {
+           alert ("¿Qué tranza?");}
+		   
+		   
+	if ( opciones.value == "adios") {
+		   alert ("¡Pues bay!") ; }
+		   
+		   
+	if ( opciones.value == "que pedo") {
+		   alert ("Uy!") ; 	}
+		   
+	if ( opciones.value == "te amo") {
+		alert ("Uo Uo Uo") ; }    
+		   
+	}	    	   
+
+}
+
+
